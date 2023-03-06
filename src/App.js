38b380d8dedb199d6c9e0c8670a31e3320264de0
Winnie Phebus <winn.phebus@gmail.com>
2023-03-06 17:38:08 -0500

    added Tailwind, returning to Design phase

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable jsx-a11y/alt-text */
 // import logo from "./logo.svg";
-import "./App.css";
+import "./dist/tailwind-app.css";
 import Avatar from "./images/gh_profile_small.png";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={Avatar} />
-        <ul>
-          <li>
+        <img class="avatar-img" src={Avatar} />
+        <ul class="tab-panel">
+          <li class="tab-item">
             <a href="#"> Write </a>
           </li>
-          <li>
+          <li class="tab-item">
             <a href="#"> Code </a>
           </li>
-          <li>
+          <li class="tab-item">
             <a href="#"> Photograph </a>
           </li>
           {/* <li><a href="#"> </a></li> */}
