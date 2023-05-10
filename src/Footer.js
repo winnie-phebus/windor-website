e253210a4faf08aeb6c953c8590c3ff3251b32ec
windorland <winn.phebus@gmail.com>
2023-05-09 21:30:04 -0400

    getting the hang of Tailwind, added basic structure for Project page

--- src/Footer.js
+++ src/Footer.js
@@ -2,8 +2,9 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className="Footer" class="container-fluid bg-slate-600">
-      <p>footer: working on this </p>
+    <div class="container-fluid mx-auto w-full flex flex-row items-center justify-center space-x-3 bg-slate-600 text-white">
+      <p>© Windorland 2023</p>
+      <p>- love, Winn</p>
     </div>
   );
 }
